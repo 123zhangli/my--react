a08274bb6b213a7f3c06f6a3f4051936d805540b
zhangli <1638742483@qq.com>
2019-09-11 18:26:16 +0800

    完成login组件的结构和样式

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,10 +1,37 @@
 import React, {Component} from 'react'
+import {Form, Input, Icon, Button, Checkbox} from 'antd';//引入UI组件库
 
+import './index.less'
+import logo from './logo.png';//引入logo图标
 export default class Login extends Component {
+
+    //自定义表单验证方法
+    
+
     render() {
         return (
-            <div>
-                login
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目：后台管理项目</h1>
+                </header>
+                <section className="login-section">
+                    <h4>用户登录界面</h4>
+                  <Form>
+                      <Form.Item>
+                          <Input prefix={<Icon type="user-add" />} placeholder="用户名" type="password"/>
+                      </Form.Item>
+                      <Form.Item>
+                          <Input prefix={<Icon type="lock" />} placeholder="密码" type="password"/>
+                      </Form.Item>
+                      <Form.Item>
+                          <Checkbox>记住密码</Checkbox>
+                      </Form.Item>
+                      <Form.Item>
+                          <Button type="dashed " htmlType="submit" className="login-btn">登录</Button>
+                      </Form.Item>
+                  </Form>
+                </section>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
+import './assets/less/index.less'//引入公共样式
 
 import store from './redux/store';
 import App from './App';
