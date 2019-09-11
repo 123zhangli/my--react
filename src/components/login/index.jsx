04748fe67052e3792ec35b0fe39af334368a57c0
zhangli <1638742483@qq.com>
2019-09-11 23:08:22 +0800

    完成login组件--登录功能1

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
-import {Form, Input, Icon, Button, Checkbox} from 'antd';//引入UI组件库
+import {Form, Input, Icon, Button, Checkbox,message} from 'antd';//引入UI组件库
+import {Redirect} from 'react-router-dom'
+import axios from 'axios'
 
 import './index.less'
-import logo from './logo.png';//引入logo图标
+import logo from './logo.png';
+
+//引入logo图标
 
 @Form.create()//装饰器语法
 class Login extends Component {
    /*
-   * 自定义表单校验
+   * 自定义表单校验函数
    * */
     validator = (rule,value,callback)=> {
         const name = rule.field === 'username' ? '用户名' : '密码';
@@ -17,8 +21,38 @@ class Login extends Component {
         const reg = /^[a-zA-Z0-9_]{3,13}$/;
         if (!reg.test(value)) {return callback(`${name}只能包含英文、数字和下划线`);}
         callback();//调用callback
-    }
-    ;
+    };
+    /*
+    登录函数
+    * */
+    login = (e)=>{
+        e.preventDefault();
+        this.props.form.validateFields((error,values)=>{
+            if (!error){
+                //校验成功
+                const {username,password} = values;//获取表单的值
+               // console.log(values);
+                axios.post('http://localhost:3000/api/login',{username,password})
+                .then((response)=>{
+                    if (response.data.status===0) {
+                       //成功
+                        message.success('登录成功');
+                        //跳转‘/’路由
+                       return <Redirect to="/"/>
+                    }else{
+                       //失败
+                        message.error(response.data.msg)
+                    }
+                })
+                .catch((error)=>{
+                    //失败
+                    message.error('网络错误');
+                })
+            }else{
+
+            }
+        });
+    };
     render() {
         const {getFieldDecorator} = this.props.form;//表单校验
         return (
@@ -29,7 +63,7 @@ class Login extends Component {
                 </header>
                 <section className="login-section">
                     <h4>用户登录界面</h4>
-                    <Form>
+                    <Form  onSubmit={this.login} >
                         <Form.Item>
                             {
                                 getFieldDecorator
@@ -57,4 +91,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default Login;//暴漏新的Login
